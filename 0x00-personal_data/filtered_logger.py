331c1f6619f1c2af36603c472bc02520c5108abd
Nduka Henry Chisom <henrysam140@gmail.com>
2023-08-03 20:20:31 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,96 +1,86 @@
 #!/usr/bin/env python3
-""" Protecting PII """
-
-from typing import List
-import logging
+""" Personal data project """
 import re
-from mysql.connector import connection
-from os import environ
+import os
+import mysql.connector
+import logging
+from typing import List
 
-PII_FIELDS = ('name', 'email', 'password', 'ssn', 'phone')
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     """ returns the log message obfuscated """
-    temp = message
-    for field in fields:
-        temp = re.sub(field + "=.*?" + separator,
-                      field + "=" + redaction + separator, temp)
-    return temp
-
-
-def get_logger() -> logging.Logger:
-    """ Returns logger obj  """
-    logger = logging.getLogger('user_data')
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
-    logger.addHandler(stream_handler)
-    return logger
-
-
-def get_db() -> connection.MySQLConnection:
-    """
-    Connect to mysql server with environmental vars
-    """
-    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
-    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    db_host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = environ.get("PERSONAL_DATA_DB_NAME")
-    connector = connection.MySQLConnection(
-        user=username,
-        password=password,
-        host=db_host,
-        database=db_name)
-    return connector
+    for i in fields:
+        message = re.sub(fr'{i}=.+?{separator}',
+                         f'{i}={redaction}{separator}', message)
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
+    """ Redacting Formatter class
+        """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """ inits class instance """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        """ constructor method """
         self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
-        """ filters values in incoming log records """
-        return filter_datum(
-            self.fields, self.REDACTION, super(
-                RedactingFormatter, self).format(record),
-            self.SEPARATOR)
+        """ filter values in a log record"""
+        return filter_datum(self.fields,
+                            self.REDACTION,
+                            super().format(record),
+                            self.SEPARATOR)
 
 
-def main() -> None:
-    """
-    Obtain a database connection using get_db
-    and retrieve all rows in the users table and display each row
-    """
-    db = get_db()
-    cur = db.cursor()
+def get_logger() -> logging.Logger:
+    """ return logging.Logger object """
+    obj = logging.getLogger("user_data")
+    obj.setLevel(logging.INFO)
+    obj.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    obj.addHandler(handler)
+    return obj
 
-    query = ('SELECT * FROM users;')
-    cur.execute(query)
-    fetch_data = cur.fetchall()
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ return the connector of the database """
+    user_name = os.getenv('PERSONAL_DATA_DB_USERNAME')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+    return mysql.connector.connect(user=user_name,
+                                   password=password,
+                                   host=host,
+                                   database=db_name)
+
+
+def main():
+    """ main function """
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * from users")
+    fields = [user[0] for user in cursor.description]
+    print(fields)
 
     logger = get_logger()
 
-    for row in fetch_data:
-        fields = 'name={}; email={}; phone={}; ssn={}; password={}; ip={}; '\
-            'last_login={}; user_agent={};'
-        fields = fields.format(row[0], row[1], row[2], row[3],
-                               row[4], row[5], row[6], row[7])
-        logger.info(fields)
+    for i in cursor:
+        list_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(i, fields))
+        logger.info(i)
 
-    cur.close()
-    db.close()
+    cursor.close()
+    conn.close()
 
 
 if __name__ == "__main__":
